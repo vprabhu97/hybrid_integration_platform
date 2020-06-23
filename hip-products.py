225c29672d8b2c1eacc1356bcc549ad6df88a6c2
vprabhu97 <vprabhu97@gmail.com>
2020-06-23 17:32:52 +0530

    added getdata function to app file

--- hip-products.py
+++ hip-products.py
@@ -2,26 +2,25 @@ from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
+data = {
+    "product1": {
+        "name": "Washing machine",
+        "species": "Germany"
+    },
+    "product2": {
+        "name": "Kite",
+        "species": "India"
+    }
+    }
 #hello function
 @app.route("/")
-def hello():
-        data = {
-                    "product1": {
-                                "name": "Washing machine",
-                                        "species": "Germany"
-                                            },
-                        "product2": {
-                                    "name": "Kite",
-                                            "species": "India"
-                                                }
-                            }   
-            return render_template('index.html', message=data)
+def hello():	
+    return render_template('index.html', message=data)
 
-
-        if __name__ == '__main__':
-                app.run(debug=True,host='0.0.0.0',port=5000)
+@app.route('/getdata')
+def hello_name():
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
-
 
